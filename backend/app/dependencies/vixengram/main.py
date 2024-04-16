9aad0b3054676353ebd83d0672e5a08f2b343c53
darklorian <lolpokens@mail.ru>
2024-04-17 02:14:00 +0400

    Create main app class

---
+++ backend/app/dependencies/vixengram/main.py
@@ -0,0 +1,29 @@
+from dependencies.vixengram.api import TelegramAPI
+from dependencies.vixengram.longpoll import LongPoll
+from dependencies.vixengram.routing import Router
+from settings import settings
+
+
+class VixenGram:
+    def __init__(self, token: str = None):
+        self.__token = token or settings.TG_TOKEN
+        self.__main_router = None
+        self.api = TelegramAPI()
+
+    async def polling(self):
+        """
+        Telegram LongPoll Method to Get Updates
+        """
+        lp = LongPoll()
+        while True:
+            message = await lp.listen()
+            if not message:
+                continue
+
+            if not isinstance(self.__main_router, Router):
+                raise AttributeError('Main router need `Router` type.')
+
+            await self.__main_router.call_handler(message)
+
+    def add_router(self, router: 'Router'):
+        self.__main_router = router
