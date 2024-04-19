6cbcc84af7a6d6fad2972cd95413ce6fb4de4926
darklorian <lolpokens@mail.ru>
2024-04-19 17:40:47 +0400

    create debug_logger and rename core to core_logger

--- backend/app/dependencies/vixengram/settings.py
+++ backend/app/dependencies/vixengram/settings.py
@@ -2,6 +2,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 i18n_logger = logging.getLogger('vixengram_i18n')
-core = logging.getLogger('vixengram_core')
+core_logger = logging.getLogger('vixengram_core')
 api_logger = logging.getLogger('vixengram_api')
 routing_logger = logging.getLogger('vixengram_routing')
+debug_logger = logging.getLogger('vixengram_debug')
