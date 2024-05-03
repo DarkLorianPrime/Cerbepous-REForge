ab486cdd3f6146b9f11d0a65a089b85eaac81d73
darklorian <lolpokens@mail.ru>
2024-05-03 12:28:56 +0400

    Add annotations, save schema to aiohttp app

--- vixengram/core.py
+++ vixengram/core.py
@@ -1,9 +1,11 @@
 from importlib.util import find_spec
+from typing import Any, Dict, List
 
 from vixengram.api import TelegramAPI
 from vixengram.backends.webhooks import WebHook, delete_webhook
 from vixengram.internationalization.i18n import I18N
 from vixengram.backends.longpoll import LongPoll
+from vixengram.pydantic_models.telegram import CallbackQueryEvent, CallbackQuery, Message
 from vixengram.routing import Router
 from vixengram.settings import routing_logger
 from vixengram.vixenapi.core import VixenAPIGenerator
@@ -20,6 +22,7 @@ class VixenGram:
             version: str = "0.0.1",
             i18n: I18N | None = None,
     ):
+        self.api_json = None
         self.title: str = title
         self.version: str = version
         self.__token: str = token
@@ -28,7 +31,13 @@ class VixenGram:
         self.__main_router: Router | None = None
         self.api = TelegramAPI()
 
-    async def webhook(self, host: str, port: int, webhook_url: str) -> None:
+    async def webhook(
+            self,
+            host: str,
+            port: int,
+            webhook_url: str,
+            api_schema: dict | None = None
+    ) -> None:
         wh = WebHook(webhook_url)
         await wh.set_webhook()
 
@@ -36,6 +45,7 @@ class VixenGram:
             raise AttributeError("Main router need `Router` type.")
 
         wh.app["main_router"] = self.__main_router
+        wh.app["api_schema"] = api_schema
         await web._run_app(wh.app, host=host, port=port)
         delete_webhook()
 
@@ -54,8 +64,9 @@ class VixenGram:
             if not isinstance(self.__main_router, Router):
                 raise AttributeError("Main router need `Router` type.")
 
+            message: CallbackQuery | CallbackQueryEvent | Message
             for message in messages:
-                if hasattr(message, "callback_query"):
+                if isinstance(message, CallbackQueryEvent):
                     message = message.callback_query
 
                 routing_logger.info(
@@ -68,7 +79,7 @@ class VixenGram:
     def add_router(self, router: Router) -> None:
         self.__main_router = router
 
-    def generate_api(self) -> None:
+    def generate_api(self) -> Dict[str, Dict[str, Any] | List[Dict[str, Any]] | str]:
         if not isinstance(self.__main_router, Router):
             raise AttributeError("Main router need `Router` type.")
 
@@ -76,10 +87,10 @@ class VixenGram:
         aiohttp = find_spec("aiohttp")
 
         if not all((jinja2, aiohttp)):
-            # raise ModuleNotFoundError(
-            "Package Jinja2 or aiohttp not found. The Docs API cannot be generated."
-        # )
+            raise ModuleNotFoundError(
+                "Package Jinja2 or aiohttp not found. The Docs API cannot be generated."
+            )
 
-        VixenAPIGenerator(
+        return VixenAPIGenerator(
             self.title, self.version, routers=self.__main_router.get_routers()
         ).generate_json()
